c90b2c67fccf7f5138bd16b156bb85c5c60c9bcf
Inframan <gabrielsouto100d@gmail.com>
2015-10-23 16:20:11 +0100

    starting webservices

---
+++ Hello World INTEG PRIMAVERA/FirstREST/FirstREST/Lib_Primavera/Model/Artigo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstREST.Lib_Primavera.Model
+{
+    public class Artigo
+    {
+        public string CodArtigo
+        {
+            get;
+            set;
+        }
+
+        public string DescArtigo
+        {
+            get;
+            set;
+        }
+
+        public double PVP1
+        {
+            get;
+            set;
+        }
+
+        public string Plataforma
+        {
+            get;
+            set;
+        }
+
+    }
+}
