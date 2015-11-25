232dcd2a988cb43b8e9ecbc89504a276181aa96e
Inframan <gabrielsouto100d@gmail.com>
2015-11-25 00:30:26 +0000

    trying ajax request front-end

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,6 +5,13 @@
 var storeControllers = angular.module('storeControllers',['ngRoute']);
 
 storeControllers.controller('StoreCtrl',function ($scope, $http){
+	$http({
+    url: "http://127.0.0.1:49822/api/artigos",
+    method: "GET",
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+  }).success(function(response) {
+    $scope.games = response;
+    console.log(response);
 });
 
 storeControllers.controller('GameCtrl', function ($scope,$http,$routeParams){
