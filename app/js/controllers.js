00625d16b94e0ee893c81e150643d76a76e90b7f
José <josecardos@gmail.com>
2015-11-26 19:02:15 +0000

    fiz cenas

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,12 +7,12 @@ var storeControllers = angular.module('storeControllers',['ngRoute']);
 storeControllers.controller('StoreCtrl', ['$scope', '$http', function ($scope, $http){
 
 	$http({
-    url: "http://127.0.0.1:49822/api/artigos",
-    method: "GET",
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-  }).success(function(response) {
-    $scope.games = response;
-});
+        url: "http://127.0.0.1:49822/api/artigos",
+        method: "GET",
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      }).success(function(response) {
+        $scope.games = response;
+    });
 
 }]);
 
@@ -21,15 +21,33 @@ storeControllers.controller('GameCtrl', ['$scope', '$http', '$routeParams', func
    
 	var s = $routeParams.artigoId;
 
-$http({
-    url: "http://127.0.0.1:49822/api/artigos/" +s,
-    method: "GET",
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-  }).success(function(response) {
-    $scope.game = response;
-    console.log(response);
-});
+    $http({
+        url: "http://127.0.0.1:49822/api/artigos/" +s,
+        method: "GET",
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      }).success(function(response) {
+        $scope.game = response;
+        console.log(response);
+    });
 
+
+}]);
+
+
+
+
+storeControllers.controller('OrderCtrl', ['$scope', '$http', function ($scope, $http){
+
+    var s = $routeParams.clienteID;
+
+    $http({
+        //  url: "http://127.0.0.1:49822/api/DocVenda/" + s,  //versão final
+        url: "http://127.0.0.1:49822/api/DocVenda/C0001",//para teste, as encomenas do cliente C0001
+        method: "GET",
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      }).success(function(response) {
+        $scope.orders = response;
+    });
 
 }]);
 
